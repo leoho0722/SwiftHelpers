95cba68363710a30b36c639a86c1de5e0df81599
leoho0722 <leo160918@gmail.com>
2023-06-23 14:11:08 +0800

    Add StringSizeCalculator String Extensions

---
+++ Sources/UIHelpers/StringSizeCalculator.swift
@@ -0,0 +1,19 @@
+//
+//  StringSizeCalculator.swift
+//  SwiftHelpers/UIHelpers
+//
+//  Created by Leo Ho on 2023/6/23.
+//
+
+import UIKit
+
+public extension String {
+    
+    /// 計算字的大小
+    /// - Parameters:
+    ///   - font: 字體
+    /// - Returns: 字的大小
+    func size(with font: UIFont) -> CGSize {
+        return NSString(string: self).size(withAttributes: [.font : font])
+    }
+}
