7cf7eb049f50e7e15f1763931d533d2ac4fe1693
leoho0722 <leo160918@gmail.com>
2023-12-10 00:13:21 +0800

    Add UIAlertController+Extensions

---
+++ Sources/SwiftHelpers/Extensions/UIAlertController+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  UIAlertController+Extensions.swift
+//  SwiftHelpers/Extensions
+//
+//  Created by Leo Ho on 2023/12/9.
+//
+
+import UIKit
+
+public extension UIAlertController {
+    
+    /// Customize the attributedMessage of the UIAlertController message
+    /// - Parameters:
+    ///   - value: attributedMessage
+    func setAttributedMessage(value: Any?) {
+       setValue(value, forKey: "attributedMessage")
+    }
+}
