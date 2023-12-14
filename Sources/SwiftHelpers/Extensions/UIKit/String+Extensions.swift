de1dafb0cccf278a83a4ebb328fb6a2911106ede
leoho0722 <leo160918@gmail.com>
2023-12-14 23:14:29 +0800

    Renamed UIImage, Image, Label initializer, and deprecate initializer of previous version

--- Sources/SwiftHelpers/Extensions/UIKit/String+Extensions.swift
+++ Sources/SwiftHelpers/Extensions/UIKit/String+Extensions.swift
@@ -16,4 +16,24 @@ public extension String {
     func size(with font: UIFont) -> CGSize {
         return NSString(string: self).size(withAttributes: [.font : font])
     }
+    
+    /// Convert hexadecimal string to Data
+    /// - Throws: NSError
+    /// - Returns: nil or `Data` with hexadecimal
+    func toHexData() throws -> Data? {
+        var data = Data(capacity: self.count / 2)
+        
+        let regex = try NSRegularExpression(pattern: "[0-9a-f]{1,2}", options: .caseInsensitive)
+        regex.enumerateMatches(in: self, range: NSMakeRange(0, utf16.count)) { match, flags, _ in
+            let byteString = (self as NSString).substring(with: match!.range)
+            var num = UInt8(byteString, radix: 16)!
+            data.append(&num, count: 1)
+        }
+        
+        guard data.count > 0 else {
+            return nil
+        }
+        
+        return data
+    }
 }
